2fa20fe1ec6824b1fac7542c6ca5406299333123
Bolshakova Anastassiya <110549895+pigodia@users.noreply.github.com>
2026-09-22 13:11:26 +0900

    Add files via upload
    4 week home work

---
+++ main_week4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    printf("Sum: %d\n", a + b);
+    printf("Diffrence: %d\n", a - b);
+    printf("Quotetion: %d\n" , a/b);
+    printf("Remainer: %d\n", a * b);
+    return 0;
+}
